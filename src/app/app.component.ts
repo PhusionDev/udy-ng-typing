a29d413b19b9e5d836e26816413a067ca8bffaee
Jared Hodgkins <phusiondev@gmail.com>
2021-05-28 13:28:08 -0400

    47. Displaying Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  sentence: string = lorem.sentence();
+
   title = 'typing';
 }
